f772670069db885bc2185af8bd54d0135ef4cb8f
Artur <Artur@email.com>
2023-11-11 11:33:33 +0200

    Change UI for Health

--- Assets/Scripts/StaminaHealth/HeathUI.cs
+++ Assets/Scripts/StaminaHealth/HeathUI.cs
@@ -10,23 +10,49 @@ public class HeathUI : MonoBehaviour
     [SerializeField] private Gradient _gradient;
     [SerializeField] private TMP_Text _text;
     [SerializeField] private string _format = "{0:0} / {1:0}";
+	[SerializeField, Min(0)] private float _updateTime = 2f;
+
+	private float _currentHealth;
+	private float _currentHealthOnUI;
+	private float _max;
+	private float _updateTimer;
 
 	private void Start()
 	{
+		_currentHealthOnUI = _currentHealth = _health.CurrentHealth;
 		UpdateUI(_health.CurrentHealth, _health.MaxHealth);
+	}
+
+	private void Update()
+	{
+		if (_currentHealthOnUI != _currentHealth)
+		{
+			float health = Mathf.Lerp(_currentHealthOnUI, _currentHealth, _updateTimer / _updateTime);
+			_updateTimer += Time.deltaTime;
+			UpdateUI(health, _max);
+			if (_updateTimer >= _updateTime)
+				_currentHealthOnUI = _currentHealth;
+		}
 	}
 
 	private void OnEnable()
 	{
-		_health .OnHealthChanged += UpdateUI;
+		_health.OnHealthChanged += SetCurrentHealth;
 	}
 
     private void OnDisable()
     {
-		_health.OnHealthChanged -= UpdateUI;
+		_health.OnHealthChanged -= SetCurrentHealth;
 	}
 
-    private void UpdateUI(float current, float max)
+	private void SetCurrentHealth(float current, float max)
+	{
+		_currentHealth = current;
+		_max = max;
+		_updateTimer = 0;
+	}
+
+	private void UpdateUI(float current, float max)
     {
         float fillAmount = current / max;
         _foreground.fillAmount = fillAmount;
